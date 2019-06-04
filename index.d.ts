1f33e767a3e97385a81d1c88659a999b524c34c8
Charujan Thavendran <charujan.thavendran@kapten.com>
2019-06-04 15:30:05 +0200

    Revert "TECH - Export childLogger & RequestId middlewares for TS projects"

--- index.d.ts
+++ index.d.ts
@@ -4,7 +4,6 @@
 // TypeScript Version: 3.0.1
 
 import { RequestHandler } from 'express';
-import { Logger } from 'chpr-logger'
 
 declare module ExpressMiddleware {
   interface Translator {
@@ -18,8 +17,6 @@ declare module ExpressMiddleware {
 
   export function i18n(translator: Translator): RequestHandler
   export function language(config: LanguageConfig): RequestHandler
-  export function requestId(): RequestHandler
-  export function childLogger(logger: Logger): RequestHandler
 }
 
 export = ExpressMiddleware;
